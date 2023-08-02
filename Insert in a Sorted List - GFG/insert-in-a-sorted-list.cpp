a39bb7ed2f9d9800f475c98e3745e3aadfb20455
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-08-03 03:16:09 +0530

    Added solution - LeetHub

---
+++ Insert in a Sorted List - GFG/insert-in-a-sorted-list.cpp
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Should return head of the modified linked list
+    Node *sortedInsert(struct Node* head, int data) {
+        Node* temp = head;
+        if(data < head->data)
+        {
+            Node* dummy = new Node(data);
+            dummy->next = head;
+            head = dummy;
+            return head;
+        }
+        
+        while(temp->next && data > temp->next->data)
+        {
+            temp = temp->next;
+        }
+        
+        if(temp->next == NULL)
+        {
+            Node* dummy = new Node(data);
+            temp->next = dummy;
+            return head;
+        }
+        
+        Node* dummy = new Node(data);
+        Node* connect = temp->next;
+        
+        temp->next = dummy;
+        dummy->next = connect;
+        
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
