5edcd8c2266130aec09e43aa41669705b47eb111
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-08-28 21:46:06 +0530

    Time: 63 ms (63.08%), Space: 34.4 MB (29.47%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,36 +1,56 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        if (mat.empty() || mat[0].empty())
-            return {};
+        int rowSize = mat.size();
+        int colSize = mat[0].size();
 
-        int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> queue;
-        int MAX_VALUE = m * n;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    queue.push({i, j});
-                } else {
-                    mat[i][j] = MAX_VALUE;
+        queue<pair<pair<int, int>, int>>q;
+        vector<vector<int>>visited(rowSize, vector<int>(colSize, 0));
+        vector<vector<int>>result(rowSize, vector<int>(colSize, 0));
+        vector<int>delRow, delCol;
+
+        delRow = {0, 0, 1, -1};
+        delCol = {1, -1, 0, 0};
+
+        for(int i=0 ;i<rowSize; i++)
+        {
+            for(int j=0; j<colSize; j++)
+            {
+                if(mat[i][j] == 0)
+                {
+                    visited[i][j] = 1;
+                    q.push({{i, j}, 0});
                 }
             }
         }
-        
-        vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        while (!queue.empty()) {
-            auto [row, col] = queue.front(); queue.pop();
-            for (auto [dr, dc] : directions) {
-                int r = row + dr, c = col + dc;
-                if (r >= 0 && r < m && c >= 0 && c < n && mat[r][c] > mat[row][col] + 1) {
-                    queue.push({r, c});
-                    mat[r][c] = mat[row][col] + 1;
+
+        while(!q.empty())
+        {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+
+            q.pop();
+            result[row][col] = steps;
+
+            for(int i=0; i<4; i++)
+            {
+                int newRow = row + delRow[i];
+                int newCol = col + delCol[i];
+
+                if(newRow >= 0 && newRow < rowSize
+                && newCol >= 0 && newCol < colSize
+                && !visited[newRow][newCol])
+                {
+                    visited[newRow][newCol] = 1;
+                    q.push({{newRow, newCol}, steps+1});
                 }
             }
+
         }
-        
-        return mat;
+
+        return result;
+
+
     }
 };
