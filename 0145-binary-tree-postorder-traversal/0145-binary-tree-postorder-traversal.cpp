908807e6f3d156b24a45dcf5987ddec2896cc768
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-09-27 23:08:18 +0530

    Time: 4 ms (34.81%), Space: 8.8 MB (19.53%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -9,31 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- #define st stack<TreeNode*>
- #define vin vector<int>
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         if(!root) return {};
-        vin ans;
-        st s1, s2;
-        s1.push(root);
-        while(!s1.empty())
+        vector<int>ans;
+        stack<TreeNode*>st;
+        
+        while(!st.empty() || root)
         {
-            auto top = s1.top();
-            s1.pop();
-            s2.push(top);
+            if(root)
+            {
+                st.push(root);
+                root = root -> left;
+            }
+            else
+            {
+                auto temp = st.top()->right;
+                if(!temp)
+                {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() and temp == st.top()->right)
+                    {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }    
+                }
+                else
+                {
+                    root = temp;
+                }
+            }
 
-            if(top->left) s1.push(top->left);
-            if(top->right) s1.push(top->right);
+            
         }
-
-        while(!s2.empty())
-        {
-            ans.push_back(s2.top()->val);
-            s2.pop();
-        }
-
         return ans;
 
     }
