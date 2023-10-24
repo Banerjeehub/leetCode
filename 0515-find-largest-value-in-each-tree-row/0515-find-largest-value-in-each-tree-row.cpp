49f81eae0a3c440968223d8660138abdf36433a9
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-10-24 23:49:32 +0530

    Time: 13 ms (24.46%), Space: 22.7 MB (19.92%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.cpp
@@ -1,28 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    std::vector<int> largestValues(TreeNode* root) {
-        if (!root) return {};
+    vector<int> largestValues(TreeNode* root) {
+        if(!root) return {};
+        vector<int>res;
         
-        std::vector<int> result;
-        std::queue<TreeNode*> queue;
-        queue.push(root);
-        
-        while (!queue.empty()) {
-            int curr_level_size = queue.size();
-            int max_val = INT_MIN;
-            
-            for (int i = 0; i < curr_level_size; ++i) {
-                TreeNode* node = queue.front();
-                queue.pop();
-                max_val = std::max(max_val, node->val);
-                
-                if (node->left) queue.push(node->left);
-                if (node->right) queue.push(node->right);
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int maxi = INT_MIN;
+            int size = q.size();
+            for(int i=0; i<size; i++)
+            {
+                auto it = q.front();
+                q.pop();
+                maxi = max(maxi, it->val);
+
+                if(it->left) q.push(it->left);
+                if(it->right) q.push(it->right);
+
             }
-            
-            result.push_back(max_val);
+            res.push_back(maxi);
         }
-        
-        return result;
+
+        return res;
     }
 };
