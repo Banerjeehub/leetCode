482d4c68d654c60247362d6628323268309ad027
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-11-10 10:30:14 +0530

    Time: 32 ms (82.37%), Space: 6.8 MB (93.19%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        
         int t1 = text1.size();
         int t2 = text2.size();
 
-        vector<vector<int>>dp(t1+1, vector<int>(t2+1, -1));
-        for(int i=0; i<=t1; i++) dp[i][0] = 0;
-        for(int j=0; j<=t2; j++) dp[0][j] = 0;
+        // Use two arrays for current and previous rows
+        vector<int> currentRow(t2 + 1, 0);
+        vector<int> previousRow(t2 + 1, 0);
 
-        for(int i=1; i<=t1; i++)
-        {
-            for(int j=1; j<=t2; j++)
-            {
-                if(text1[i-1] == text2[j-1])
-                {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        for (int i = 1; i <= t1; i++) {
+            // Swap currentRow and previousRow at the beginning of each outer loop iteration
+            swap(currentRow, previousRow);
+
+            for (int j = 1; j <= t2; j++) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    // If the characters match, extend the LCS by 1
+                    currentRow[j] = 1 + previousRow[j - 1];
+                } else {
+                    // If the characters don't match, take the maximum of the LCS without considering one character from each string
+                    currentRow[j] = max(currentRow[j - 1], previousRow[j]);
                 }
             }
         }
 
-        return dp[t1][t2];
+        // The final result is stored in the last element of the currentRow array
+        return currentRow[t2];
     }
 };
