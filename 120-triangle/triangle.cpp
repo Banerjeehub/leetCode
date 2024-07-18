4fbd0f3ec933edc164d8a44bc811ac034799f8c8
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-19 00:43:21 +0530

    Time: 3 ms (82.82%) | Memory: 11.3 MB (61.31%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp)
-    {
-        if(i == triangle.size()-1) return triangle[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int d = triangle[i][j] + f(i+1, j, triangle, dp);
-        int dg = triangle[i][j] + f(i+1, j+1, triangle, dp);
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n = tri.size();
+        vector<int>dp(n, 0);
 
-        return dp[i][j] = min(d, dg);
-    }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        // vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), -1));
-        // return f(0, 0, triangle, dp);
-        int n = triangle.size();
-        vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), 0));
-        for(int i=0; i<triangle.size(); i++) dp[triangle.size()-1][i] = triangle[triangle.size()-1][i];
+        for(int i=0; i<n; i++) dp[i] = tri[n-1][i];
 
         for(int i=n-2; i>=0; i--)
         {
-            for(int j=i; j>=0; j--)
+            for(int j=0; j<=i; j++)
             {
-                int d = triangle[i][j] + dp[i+1][j];
-                int dg = triangle[i][j] + dp[i+1][j+1];
-
-                dp[i][j] = min(d, dg);
+                dp[j] = tri[i][j] + min(dp[j], dp[j+1]);
             }
         }
 
-        return dp[0][0];
-
+        return dp[0];
     }
 };
