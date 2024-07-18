4469987991fbd10a490613f3fdb030793f4be46b
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-18 08:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (6.66%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int f(int index, vector<int>&arr, vector<int>&dp)
+    {
+        if(index == 0) return arr[index];
+        if(index < 0) return 0;
+        if(dp[index] != -1) return dp[index];
+        int pick = arr[index] + f(index - 2, arr, dp);
+        int notP = f(index-1, arr, dp);
+
+        return dp[index] = max(pick, notP);
+    }
+    int rob(vector<int>& nums) {
+        int index = nums.size();
+         if(index == 1) return nums[0];
+
+        vector<int>first, second, dp1(nums.size(), -1), dp2(nums.size(), -1);
+
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(i != 0) first.push_back(nums[i]);
+            if(i != nums.size() - 1) second.push_back(nums[i]);
+        }
+
+
+        return max(f(index-2, first, dp1), f(index-2, second, dp2));
+
+    }
+};
