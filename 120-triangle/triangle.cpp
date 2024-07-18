6047dc2249508475b940f93da6589ac15408f611
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-18 20:29:13 +0530

    Time: 6 ms (35.26%) | Memory: 11.8 MB (7.41%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int f(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp)
+    {
+        if(i == triangle.size()-1) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int d = triangle[i][j] + f(i+1, j, triangle, dp);
+        int dg = triangle[i][j] + f(i+1, j+1, triangle, dp);
+
+        return dp[i][j] = min(d, dg);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), -1));
+        return f(0, 0, triangle, dp);
+    }
+};
