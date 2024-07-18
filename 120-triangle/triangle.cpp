0ca6a9d454d288b5d3858d2aac0b663dbd625811
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-18 20:40:03 +0530

    Time: 4 ms (73.09%) | Memory: 11.7 MB (7.41%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -10,7 +10,24 @@ public:
         return dp[i][j] = min(d, dg);
     }
     int minimumTotal(vector<vector<int>>& triangle) {
-        vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), -1));
-        return f(0, 0, triangle, dp);
+        // vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), -1));
+        // return f(0, 0, triangle, dp);
+        int n = triangle.size();
+        vector<vector<int>>dp(triangle.size(), vector<int>(triangle.size(), 0));
+        for(int i=0; i<triangle.size(); i++) dp[triangle.size()-1][i] = triangle[triangle.size()-1][i];
+
+        for(int i=n-2; i>=0; i--)
+        {
+            for(int j=i; j>=0; j--)
+            {
+                int d = triangle[i][j] + dp[i+1][j];
+                int dg = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(d, dg);
+            }
+        }
+
+        return dp[0][0];
+
     }
 };
