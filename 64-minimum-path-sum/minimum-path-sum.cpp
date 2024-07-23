f2048929ec357ed82fd1524b9844cf5b5182c83b
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-23 23:13:52 +0530

    Time: 10 ms (26.11%) | Memory: 12.5 MB (67.91%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -11,28 +11,28 @@ public:
         return dp[i][j] =  min(left, top);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>>dp(n, vector<int>(m, 0));
-        dp[0][0] = grid[0][0];
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<int> dp(m, 0);
 
-        for(int i=1; i<m; i++)
-        {
-            dp[0][i] = dp[0][i-1] + grid[0][i];
-        }
-        for(int i=1; i<n; i++)
-        {
-            dp[i][0] = dp[i-1][0] + grid[i][0];
-        }
+    // Initialize the first cell
+    dp[0] = grid[0][0];
 
-        for(int i=1; i<n; i++)
-        {
-            for(int j=1; j<m; j++)
-            {
-                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-
-        return dp[n-1][m-1];
+    // Initialize the first row
+    for (int j = 1; j < m; j++) {
+        dp[j] = dp[j-1] + grid[0][j];
     }
+
+    // Process the rest of the grid
+    for (int i = 1; i < n; i++) {
+        // Update the first cell of the current row
+        dp[0] += grid[i][0];
+        for (int j = 1; j < m; j++) {
+            dp[j] = grid[i][j] + min(dp[j], dp[j-1]);
+        }
+    }
+
+    return dp[m-1];
+}
+
 };
