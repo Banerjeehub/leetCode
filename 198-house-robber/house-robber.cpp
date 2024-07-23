2cfb4d5435a530a4d2fad0f5562f5849e6cfab89
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-23 10:16:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (11.82%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    int f(int index, vector<int>&nums, vector<int>&dp)
+    int f(int i, vector<int>&nums, vector<int>&dp)
     {
-        if(index == 0) return nums[index];
-        if(index < 0) return 0;
-        if(dp[index] != -1) return dp[index];
-        int pick = nums[index] + f(index-2, nums, dp);
-        int notPick = 0 + f(index-1, nums, dp);
+        if(i == 0) return nums[i];
+        if(i < 0) return 0;
+        if(dp[i] != -1) return dp[i];
+        int take = nums[i] + f(i-2, nums, dp);
+        int two = f(i-1, nums, dp);
 
-        return dp[index] = max(pick, notPick);
+        return dp[i] = max(take, two);
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-        
-        vector<int> dp(n, 0);
-        
-        // Base cases
-        int prev1 = nums[0];
-        int prev2 = max(nums[0], nums[1]);
-        
-        // Fill the dp array
-        for (int i = 2; i < n; i++) {
-            int curr = max(nums[i] + prev1, prev2);
-            prev1 = prev2;
-            prev2 = curr;
-        }
-        
-        return prev2;
+        vector<int>dp(n, -1);
+        return f(n-1, nums, dp);
     }
 };
