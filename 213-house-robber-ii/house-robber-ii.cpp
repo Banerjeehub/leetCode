3077057f0603eff69e28cacda0f69897e5601535
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-23 13:14:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int f(int index, vector<int>&arr, vector<int>&dp)
+    int f(int i, vector<int>&nums, vector<int>&dp)
     {
-        if(index == 0) return arr[index];
-        if(index < 0) return 0;
-        if(dp[index] != -1) return dp[index];
-        int pick = arr[index] + f(index - 2, arr, dp);
-        int notP = f(index-1, arr, dp);
+        if(i == 0) return nums[i];
+        if(i < 0) return 0;
+        if(dp[i] != -1) return dp[i];
+        int take = nums[i] + f(i-2, nums,dp);
+        int notTake = f(i-1, nums, dp);
 
-        return dp[index] = max(pick, notP);
+        return dp[i] = max(take, notTake);
     }
     int rob(vector<int>& nums) {
-        int index = nums.size();
-         if(index == 1) return nums[0];
-
-        vector<int>first, second, dp1(nums.size(), -1), dp2(nums.size(), -1);
+        if(nums.size() == 1) return nums[0];
+        int s = nums.size();
+        vector<int>nums1, nums2;
 
         for(int i=0; i<nums.size(); i++)
         {
-            if(i != 0) first.push_back(nums[i]);
-            if(i != nums.size() - 1) second.push_back(nums[i]);
+            if(i != 0) nums1.push_back(nums[i]);
+            if(i != nums.size()-1) nums2.push_back(nums[i]);
         }
-
-
-        return max(f(first.size()-1, first, dp1), f(second.size()-1, second, dp2));
-
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<int>dp1(m, -1), dp2(n, -1);
+        return max(f(m-1,nums1, dp1), f(n-1, nums2, dp2));
     }
 };
