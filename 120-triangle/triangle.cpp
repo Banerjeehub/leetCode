93e91d0640022247352170e310c986cc261ed1fa
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-24 09:28:17 +0530

    Time: 9 ms (11.32%) | Memory: 11.8 MB (10.75%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
+    int f(int i, int j, vector<vector<int>>& tri, vector<vector<int>>&dp)
+    {
+        if(i == tri.size() - 1) return tri[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int d = tri[i][j] + f(i+1, j, tri, dp);
+        int dia = tri[i][j] +  f(i+1, j+1, tri, dp);
+
+        return dp[i][j] = min(d, dia);
+    }
     int minimumTotal(vector<vector<int>>& tri) {
         int n = tri.size();
-        vector<int>dp(n, 0);
-
-        for(int i=0; i<n; i++) dp[i] = tri[n-1][i];
-
-        for(int i=n-2; i>=0; i--)
-        {
-            for(int j=0; j<=i; j++)
-            {
-                dp[j] = tri[i][j] + min(dp[j], dp[j+1]);
-            }
-        }
-
-        return dp[0];
+        int m = tri[0].size();
+        vector<vector<int>>dp(n, vector<int>(n, -1));
+        return f(0, 0, tri, dp);
     }
 };
