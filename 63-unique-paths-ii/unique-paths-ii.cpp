2d8e5bb937c361931dc607bd1dfad1c7b86f4c41
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-24 10:56:09 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (10.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int f(int i, int j, vector<vector<int>>& grid, vector<vector<int>>&dp)
     {
-        if(i>=0 && j>=0 && grid[i][j] == 1) return 0;
         if(i < 0 || j < 0) return 0;
+        if(grid[i][j] == 1) return 0;
         if(i == 0 && j == 0) return 1;
         if(dp[i][j] != -1) return dp[i][j];
         int top = f(i-1, j, grid, dp);
