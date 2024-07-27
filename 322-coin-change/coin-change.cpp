82253ab66d008785726d97dfcef08fd32fcc0324
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-27 11:31:19 +0530

    Time: 39 ms (51.72%) | Memory: 20.7 MB (49.32%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -15,31 +15,33 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount + 1, 0));
+        //vector<vector<int>>dp(n, vector<int>(amount + 1, 0));
+        vector<int>prev(amount+1, 0), curr(amount+1, 0);
 
         for(int i=0; i<=amount; i++)
         {
             if(i % coins[0] == 0)
             {
-                dp[0][i] = i / coins[0];
+                prev[i] = i / coins[0];
             }
-            else dp[0][i] = 1e9;
+            else prev[i] = 1e9;
         }
 
         for(int i=1; i<n; i++)
         {
             for(int j=0; j<=amount; j++)
             {
-                int notTake = dp[i-1][j];
+                int notTake = prev[j];
                 int take = INT_MAX;
-                if(j >= coins[i]) take = 1 + dp[i][j - coins[i]];
+                if(j >= coins[i]) take = 1 + curr[j - coins[i]];
 
-                dp[i][j] = min(take, notTake);
+                curr[j] = min(take, notTake);
             }
+            prev = curr;
         }
 
-        if(dp[n-1][amount] >= 1e9) return -1;
-        return dp[n-1][amount];
+        if(prev[amount] >= 1e9) return -1;
+        return prev[amount];
 
 
 
@@ -48,8 +50,8 @@ public:
 
 
 
-        int ans =  f(n-1, amount, coins, dp);
-        if(ans >= 1e9) return -1;
-        return ans;
+        // int ans =  f(n-1, amount, coins, dp);
+        // if(ans >= 1e9) return -1;
+        // return ans;
     }
 };
