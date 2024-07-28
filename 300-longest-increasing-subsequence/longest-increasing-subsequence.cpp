7b8555bc282bb5249e16a03761ba5e470ab59a75
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-28 22:48:32 +0530

    Time: 134 ms (46.12%) | Memory: 13.4 MB (43.70%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int>dp(n, 1);
+        vector<int>dp(n, 1), hash(n);
         int maxi = 1;
+        int lastIdx = 0;
         for(int i=0; i<n; i++)
         {
+            hash[i] = i;
             for(int j=0; j<i; j++)
             {
-                if(nums[i] > nums[j])
+                if(nums[i] > nums[j] && (1+dp[j] > dp[i]))
                 {
-                    dp[i] = max(dp[i], 1 + dp[j]);
+                    dp[i] = 1 + dp[j];
+                    hash[i] = j;
 
                 }
-                maxi = max(maxi, dp[i]);
+                if(maxi < dp[i])
+                {
+                    maxi = dp[i];
+                    lastIdx = i;
+                }
             }
         }
+
+        vector<int>li;
+        li.push_back(nums[lastIdx]);
+        while(hash[lastIdx] != lastIdx)
+        {
+            lastIdx = hash[lastIdx];
+            li.push_back(nums[lastIdx]);
+        }
+
+        reverse(li.begin(), li.end());
+
+        for(auto it : li) cout << it << " ";
+        cout << endl;
+
+
 
         return maxi;
     }
