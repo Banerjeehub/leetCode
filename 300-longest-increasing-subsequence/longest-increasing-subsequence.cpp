2fb639a69b185ea8b47b094864440c1a54b1f90d
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-28 19:46:00 +0530

    Time: 217 ms (39.93%) | Memory: 294.9 MB (8.55%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    int f(int idx, int prev,int n,  vector<int>&arr, vector<vector<int>>&dp)
-    {
-        if(idx == n) return 0;
-        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
-        int notTake = f(idx+1, prev,n, arr, dp);
+    int f(int idx, int prev, int n, vector<int>& arr, vector<vector<int>>& dp) {
+        if (idx == n)
+            return 0;
+        if (dp[idx][prev + 1] != -1)
+            return dp[idx][prev + 1];
+        int notTake = f(idx + 1, prev, n, arr, dp);
         int take = INT_MIN;
-        if(prev == -1 || arr[prev] < arr[idx]) take = 1 + f(idx+1, idx, n, arr, dp);
+        if (prev == -1 || arr[prev] < arr[idx])
+            take = 1 + f(idx + 1, idx, n, arr, dp);
 
-        return dp[idx][prev+1] =  max(take, notTake);
+        return dp[idx][prev + 1] = max(take, notTake);
     }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0)); // dp[i][j] means the LIS from index i with the previous index being j
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-        for (int idx = n-1; idx >= 0; idx--) {
-            for (int prev = idx-1; prev >= -1; prev--) {
-                int notTake = dp[idx+1][prev+1]; // when not taking the current element
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int prev = idx - 1; prev >= -1; prev--) {
+                int notTake = dp[idx + 1][prev + 1];
                 int take = 0;
                 if (prev == -1 || nums[idx] > nums[prev]) {
-                    take = 1 + dp[idx+1][idx+1]; // when taking the current element
+                    take = 1 + dp[idx+1][idx+1];
                 }
-                dp[idx][prev+1] = max(take, notTake);
+                dp[idx][prev + 1] = max(take, notTake);
             }
         }
 
-        return dp[0][0]; // LIS length starting from the first index
+        return dp[0][0];
     }
 };
