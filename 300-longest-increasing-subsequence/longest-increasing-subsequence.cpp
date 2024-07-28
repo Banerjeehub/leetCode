477dfb1a14e3b864eedad9a4850bda17b0cb28bd
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-28 22:21:40 +0530

    Time: 486 ms (25.66%) | Memory: 295.1 MB (8.55%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>dp(n, 1);
-        int maxi = 1;
-        for(int i=0; i<n; i++)
+    int f(int idx, int prev, int n, vector<int>&nums, vector<vector<int>>&dp)
+    {
+        if(idx == n) return 0;
+        if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
+        int notTake = f(idx+1, prev, n, nums, dp);
+        int take = 0;
+        if(prev == -1 || nums[prev] < nums[idx])
         {
-            for(int prev=0; prev<i; prev++)
-            {
-                if(nums[i] > nums[prev])
-                {
-                    dp[i] = max(dp[i], 1 + dp[prev]);
-                    maxi = max(maxi, dp[i]);
-                }
-            }
+            take = 1 + f(idx+1, idx, n, nums, dp);
         }
 
-        return maxi;
+        return dp[idx][prev+1] = max(take, notTake);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>>dp(n, vector<int>(n+1, -1));
+        return f(0, -1, n, nums, dp);
     }
 };
