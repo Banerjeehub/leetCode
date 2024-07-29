3154265e7d28f7a1b3a8ef5994d86d6f6446b2c4
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-07-29 20:14:15 +0530

    Time: 3 ms (88.53%) | Memory: 18 MB (24.51%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -18,7 +18,31 @@ public:
         return dp[idx][lib] = profit;
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
-        return f(0, 1, prices, dp);
+        vector<vector<int>>dp(prices.size()+1, vector<int>(2, 0));
+        //return f(0, 1, prices, dp);
+
+        dp[prices.size()][0] = dp[prices.size()][1] = 0;
+
+        for(int i=prices.size()-1; i>=0; i--)
+        {
+            for(int j=0; j<=1; j++)
+            {
+                int profit = 0;
+                if(j)
+                {
+                    profit = max(-prices[i]+dp[i+1][0],
+                    dp[i+1][1]);
+                }
+                else profit = max(prices[i]+dp[i+1][1],
+                dp[i+1][0]);
+
+                dp[i][j] = profit;
+            }
+        }
+
+        return dp[0][1];
+
+
+
     }
 };
