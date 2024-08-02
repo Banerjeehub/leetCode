0a25f660f0f82a69735e4876ac49085c19fcbc85
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-02 11:07:29 +0530

    Time: 89 ms (58.49%) | Memory: 23.7 MB (53.07%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -3,10 +3,11 @@ public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
-        greater<pair<int, pair<int, int>>>>pq;
+        priority_queue<pair<int, pair<int, int>>,
+                       vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>> pq;
 
-        vector<vector<int>>diff(n, vector<int>(m, 1e9));
+        vector<vector<int>> diff(n, vector<int>(m, 1e9));
 
         pq.push({0, {0, 0}});
         diff[0][0] = 0;
@@ -14,27 +15,24 @@ public:
         int dr[] = {0, 0, 1, -1};
         int dc[] = {1, -1, 0, 0};
 
-        while(!pq.empty())
-        {
+        while (!pq.empty()) {
             auto top = pq.top();
             int effort = top.first;
             int row = top.second.first;
             int col = top.second.second;
             pq.pop();
 
-            
-            if(row == n-1 && col == m-1) return effort;
+            if (row == n - 1 && col == m - 1)
+                return effort;
 
-            for(int i=0; i<4; i++)
-            {
+            for (int i = 0; i < 4; i++) {
                 int nr = row + dr[i];
                 int nc = col + dc[i];
 
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m)
-                {
-                    int newEffort = max(abs(heights[row][col] - heights[nr][nc]), effort);
-                    if(newEffort < diff[nr][nc])
-                    {
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+                    int newEffort =
+                        max(abs(heights[row][col] - heights[nr][nc]), effort);
+                    if (newEffort < diff[nr][nc]) {
                         diff[nr][nc] = newEffort;
                         pq.push({newEffort, {nr, nc}});
                     }
@@ -43,6 +41,5 @@ public:
         }
 
         return 0;
-
     }
 };
