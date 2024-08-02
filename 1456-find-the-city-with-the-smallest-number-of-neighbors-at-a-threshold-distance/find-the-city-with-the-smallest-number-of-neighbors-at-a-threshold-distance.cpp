b41c4cd3fc42068296e277008a1f99e35ac35139
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-02 22:23:08 +0530

    Time: 24 ms (73.31%) | Memory: 15.9 MB (48.83%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,51 +1,52 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>>dist(n, vector<int>(n, INT_MAX));
+        
+        vector<vector<int>>dist(n, vector<int>(n, 1e9));
         
         for(auto it : edges)
         {
             dist[it[0]][it[1]] = it[2];
             dist[it[1]][it[0]] = it[2];
         }
-        
+
         for(int i=0; i<n; i++) dist[i][i] = 0;
-        
-        
-        for(int k=0; k<n; k++)
+
+        for(int via=0; via<n; via++)
         {
             for(int i=0; i<n; i++)
             {
                 for(int j=0; j<n; j++)
                 {
-                    if(dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) continue;
+                    if(dist[i][via] == 1e9 || dist[via][j] == 1e9) continue;
                     
-                    dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
+                    dist[i][j] = min(dist[i][j], dist[i][via] + dist[via][j]);
+                    
                 }
             }
         }
-        
-        int cityCnt = n;
-        int resCity = -1;
-        
+
+        int count = n;
+        int res = -1;
         for(int i=0; i<n; i++)
         {
-            int cnt = 0;
+            int temp = 0;
             for(int j=0; j<n; j++)
             {
                 if(dist[i][j] <= distanceThreshold)
                 {
-                    cnt++;
+                    temp++;
                 }
             }
-            
-            if(cnt <= cityCnt)
+
+            if(temp <= count)
             {
-                cityCnt = cnt;
-                resCity = i;
+                count = temp;
+                res = i;
+
             }
         }
-        
-        return resCity;
+
+        return res;
     }
 };
