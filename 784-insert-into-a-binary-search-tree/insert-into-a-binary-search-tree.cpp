90b8e7318c2a386e3ef56452ff0fd647306275ee
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-04 20:00:09 +0530

    Time: 77 ms (6.26%) | Memory: 57.6 MB (22.61%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -6,33 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(!root) return new TreeNode(val);
-        TreeNode* curr = root;
-        while (curr) {
-            if (curr->val <= val) {
-                if (curr->right)
-                    curr = curr->right;
-                else {
-                    curr->right = new TreeNode(val);
-                    break;
-                }
-            } else {
-                if (curr->left)
-                    curr = curr->left;
-                else {
-                    curr->left = new TreeNode(val);
-                    break;
-                }
-            }
+        // If the current node is null, create a new node with the given value
+        if (!root) {
+            return new TreeNode(val);
         }
-
+        
+        // Recursively insert the value in the correct position
+        if (val < root->val) {
+            root->left = insertIntoBST(root->left, val);
+        } else {
+            root->right = insertIntoBST(root->right, val);
+        }
+        
+        // Return the unchanged root node
         return root;
     }
 };
