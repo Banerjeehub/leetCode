fc4c76d6d37b31330c3a1176b340f121d9812135
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-04 21:52:28 +0530

    Time: 11 ms (36.04%) | Memory: 20.4 MB (25.09%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,16 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    // Helper function to validate BST with min and max constraints
-    bool validate(TreeNode* root, long long min, long long max) {
-        if (!root) return true;
-        if (root->val >= max || root->val <= min) return false;
+    bool f(TreeNode* root, long long min, long long max)
+    {
+        if(!root) return true;
+        if(root->val >= max || root->val <= min) return false;
 
-        // Check the left and right subtrees with updated constraints
-        return validate(root->left, min, root->val) && validate(root->right, root->val, max);
+        return f(root->left, min, root->val) && f(root->right, root->val , max);
     }
-
-    // Main function to check if the tree is a valid BST
     bool isValidBST(TreeNode* root) {
-        return validate(root, std::numeric_limits<long long>::min(), std::numeric_limits<long long>::max());
+        
+        return f(root, LONG_MIN, LONG_MAX);
     }
 };
