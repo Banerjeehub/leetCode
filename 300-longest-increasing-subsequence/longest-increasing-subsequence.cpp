c59c15e2010023462e19cd23c31652405b539afd
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-06 18:03:49 +0530

    Time: 525 ms (14.21%) | Memory: 295 MB (6.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -5,12 +5,14 @@ public:
         if(idx == nums.size()) return 0;
 
         if(dp[idx][prev+1] != -1) return dp[idx][prev+1];
+
         int notTake = f(idx+1, prev, nums, dp);
         int take = 0;
-        if(prev == -1 || nums[idx] > nums[prev])
+        if(prev == -1 || nums[prev] < nums[idx])
         {
             take = 1 + f(idx+1, idx, nums, dp);
         }
+
         return dp[idx][prev+1] = max(take, notTake);
     }
     int lengthOfLIS(vector<int>& nums) {
