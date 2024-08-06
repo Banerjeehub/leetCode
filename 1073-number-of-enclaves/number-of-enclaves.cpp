5162fde5a80ceacb487d90dd21adc92c5fc60bb1
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-07 00:03:12 +0530

    Time: 51 ms (58.17%) | Memory: 35.8 MB (37.93%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,61 +1,62 @@
 class Solution {
 public:
-    void dfs(int r, int c, vector<vector<int>>& visited, vector<vector<int>>& grid)
+    void dfs(int i, int j, vector<vector<int>>&grid, vector<vector<int>>&visited)
     {
         int n = grid.size();
         int m = grid[0].size();
+        visited[i][j] = 1;
 
         int dr[] = {0, 0, 1, -1};
         int dc[] = {1, -1, 0, 0};
 
-        visited[r][c] = 1;
-
-        for(int i=0; i<4; i++)
+        for(int id=0; id<4; id++)
         {
-            int nr = r + dr[i];
-            int nc = c + dc[i];
+            int nr = i + dr[id];
+            int nc = j + dc[id];
 
             if(nr >= 0 && nr < n && nc >= 0 && nc < m && !visited[nr][nc] && grid[nr][nc] == 1)
             {
-                dfs(nr, nc, visited, grid);
+                dfs(nr, nc, grid, visited);
             }
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-
-        vector<vector<int>>visited(n, vector<int>(m, 0));
-        for(int i=0; i<m; i++)
-        {
-            if(!visited[0][i] && grid[0][i] == 1)
-            {
-                dfs(0, i, visited, grid);
-            }
-            if(!visited[n-1][i] && grid[n-1][i] == 1)
-            {
-                dfs(n-1, i, visited, grid);
-            }
-        }
+        vector<vector<int>>visited(n, vector<int>(m ,0));
         for(int i=0; i<n; i++)
         {
-            if(!visited[i][0] && grid[i][0] == 1)
+            if(grid[i][0] == 1 && !visited[i][0])
             {
-                dfs(i, 0, visited, grid);
+                dfs(i, 0, grid, visited);
             }
-            if(!visited[i][m-1] && grid[i][m-1] == 1)
+            if(grid[i][m-1] == 1 && !visited[i][m-1])
             {
-                dfs(i, m-1, visited, grid);
+                dfs(i, m-1, grid, visited);
             }
         }
 
-        int count = 0;
+        for(int i=0; i<m; i++)
+        {
+            if(grid[0][i] == 1 && !visited[0][i])
+            {
+                dfs(0, i, grid, visited);
+            }
 
+            if(grid[n-1][i] == 1 && !visited[n-1][i])
+            {
+                dfs(n-1, i, grid, visited);
+            }
+        }
+        int count = 0;
         for(int i=0; i<n; i++)
         {
             for(int j=0; j<m; j++)
             {
-                if(!visited[i][j] && grid[i][j] == 1) count++;
+                if(!visited[i][j] && grid[i][j] == 1)
+                {
+                    count++;
+                }
             }
         }
 
