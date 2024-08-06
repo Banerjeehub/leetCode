4d638a641e881efcb7a15766cbc0ca5006aa7a89
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-06 09:43:45 +0530

    Time: 4 ms (39.00%) | Memory: 8.7 MB (44.18%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -6,12 +6,12 @@ public:
 
         for(int i=0; i<pow(2, nums.size()); i++)
         {
-            vector<int>st;
+            vector<int>temp;
             for(int j=0; j<nums.size(); j++)
             {
-                if(i & (1 << j)) st.push_back(nums[j]);
+                if(i & (1 << j)) temp.push_back(nums[j]);
             }
-            ans.push_back(st);
+            ans.push_back(temp);
         }
 
         return ans;
