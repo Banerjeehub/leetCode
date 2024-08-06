365498f051be66171f61e92322a5936ba3f330a5
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-06 18:41:43 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (10.08%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>&dp)
-    {
-        if(i < 0 || j < 0) return 0;
-        if(i == 0 && j == 0) return 1;
-
-        if(dp[i][j]  != -1) return dp[i][j];
-
-        int top = f(i-1, j, dp);
-        int left = f(i, j-1, dp);
-
-        return dp[i][j] = top + left;
-    }
     int uniquePaths(int m, int n) {
-        
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, dp);
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        // Base case: There's only one way to reach any cell in the first row (all moves are to the right)
+        for (int i = 0; i < m; ++i) {
+            dp[i][0] = 1;
+        }
+
+        // Base case: There's only one way to reach any cell in the first column (all moves are down)
+        for (int j = 0; j < n; ++j) {
+            dp[0][j] = 1;
+        }
+
+        // Fill the DP table
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
