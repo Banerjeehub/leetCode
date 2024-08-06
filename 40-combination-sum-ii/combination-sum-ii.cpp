bde5070c83fa174bec7ddd0f529cc3b03170297f
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-06 11:16:01 +0530

    Time: 4 ms (60.93%) | Memory: 12.9 MB (52.09%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
-public:
-    void f(int i, vector<int>& temp, vector<int>& arr, vector<vector<int>>& ans, int target) {
+private:
+    void f(int idx, vector<int>& arr, int target, vector<int>& temp,
+           vector<vector<int>>& ans) {
         if (target == 0) {
             ans.push_back(temp);
             return;
         }
+        if (target < 0)
+            return;
 
-        if (target < 0) return;
-
-        for (int idx = i; idx < arr.size(); idx++) {
-            // Skip duplicates
-            if (idx > i && arr[idx] == arr[idx - 1]) continue;
-            temp.push_back(arr[idx]);
-            f(idx + 1, temp, arr, ans, target - arr[idx]);
+        for (int i = idx; i < arr.size(); i++) {
+            if( i > idx && arr[i] == arr[i-1]) continue;
+            temp.push_back(arr[i]);
+            f(i + 1, arr, target - arr[i], temp, ans);
             temp.pop_back();
         }
     }
 
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+
         vector<vector<int>> ans;
         vector<int> temp;
-
-        // Sort the candidates to handle duplicates
-        sort(candidates.begin(), candidates.end());
-
-        f(0, temp, candidates, ans, target);
+        sort(arr.begin(), arr.end());
+        f(0, arr, target, temp, ans);
         return ans;
     }
 };
