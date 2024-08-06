b0111720b0925a0fc63c55095f829eed32c1f48f
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-06 11:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (6.66%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,26 +1,30 @@
 class Solution {
-public:
-    vector<vector<int>> ans;
-    void fun(vector<int>& v, int k, int sum, int i){
-        if(sum==0 && k==0){
-            ans.push_back(v);
+private:
+    void getCombinations(int idx, int liberty, int target, vector<vector<int>>&ans, vector<int>temp)
+    {
+        if(liberty == 0 && target == 0)
+        {
+            ans.push_back(temp);
             return;
         }
-        if(i>9 || sum<0 || k<0){
-            return ;
-        }
-        if(sum-i>=0){
-            fun(v,k,sum,i+1);
-            v.push_back(i);
-            fun(v,k-1,sum-i,i+1);
-            v.pop_back();
+        if(liberty < 0 || target < 0 || idx > 9) return;
+
+        if(target >= idx) // target - idx >= 0
+        {
+            getCombinations(idx+1, liberty, target, ans, temp);
+            temp.push_back(idx);
+            getCombinations(idx+1, liberty-1, target - idx, ans, temp);
+            temp.pop_back();
         }
     }
+public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        if(n>55)
-            return ans;
-        vector<int> v;
-        fun(v,k,n,1);
+        
+        vector<vector<int>>ans;
+        vector<int>temp;
+
+        getCombinations(1, k, n, ans, temp);
+
         return ans;
     }
 };
