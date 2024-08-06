f5b75be22af4134a454155b00d3bd3774aea1cb0
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-06 21:43:15 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (23.53%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,49 +1,23 @@
 class Solution {
 public:
-    int f(int idx, int lib, vector<int>&prices, vector<vector<int>>&dp)
+    int f(int idx, int n, int liberty, vector<int>&prices, vector<vector<int>>&dp)
     {
-        if(idx >= prices.size()) return 0;
-        if(dp[idx][lib] != -1) return dp[idx][lib];
-
+        if(idx >= n) return 0;
+        if(dp[idx][liberty] != -1) return dp[idx][liberty];
         int profit = 0;
-        if(lib)
+        if(liberty)
         {
-            profit = max(-prices[idx]+f(idx+1, 0, prices, dp), f(idx+1, 1, prices, dp));
+            profit = max(-prices[idx] + f(idx+1, n, 0, prices, dp), f(idx+1, n, liberty, prices, dp));
         }
         else
         {
-            profit = max(prices[idx] + f(idx+2, 1, prices, dp), f(idx+1, 0, prices, dp));
+            profit = max(prices[idx] + f(idx+2, n, 1, prices, dp), f(idx+1, n, liberty, prices, dp));
         }
-
-        return dp[idx][lib] = profit;
+        return dp[idx][liberty] = profit;
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-
-        vector<vector<int>>dp(n+2, vector<int>(2, 0));
-
-        //return f(0, 1, prices, dp);
-
-        for(int idx = n-1; idx>=0; idx--)
-        {
-            for(int lib=0; lib<=1; lib++)
-            {
-                int profit = 0;
-                if(lib)
-                {
-                    profit = max(-prices[idx]+dp[idx+1][0], dp[idx+1][1]);
-                }
-                else
-                {
-                    profit = max(prices[idx] + dp[idx+2][1], dp[idx+1][0]);
-                }
-
-                dp[idx][lib] = profit;
-            }
-        }
-
-        return dp[0][1];
-
-
+        vector<vector<int>>dp(n, vector<int>(2, -1));
+        return f(0, n, 1, prices, dp);
     }
 };
