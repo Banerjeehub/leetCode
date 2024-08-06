889023685bba445a29360bf9fd16c7c15fe63701
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-06 12:49:46 +0530

    Time: 54 ms (20.84%) | Memory: 47.6 MB (24.20%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,57 +1,27 @@
 class Solution {
 public:
-    int f(int i, int tar, vector<int>&coins, vector<vector<int>>&dp)
+    int f(int i, int amount, vector<int>&coins, vector<vector<int>>&dp)
     {
         if(i == 0)
         {
-            if(tar % coins[0] == 0) return tar / coins[0];
-            else return 1e9;
+            if(amount % coins[0] == 0) return amount/coins[0];
+            else return 1e7;
         }
-        if(dp[i][tar] != -1) return dp[i][tar];
-        int notTake = f(i-1, tar, coins, dp);
+        if(dp[i][amount] != -1) return dp[i][amount];
+        int notTake = f(i-1, amount, coins, dp);
         int take = INT_MAX;
-        if(coins[i] <= tar) take = 1 + f(i, tar - coins[i], coins, dp);
-        return dp[i][tar] =  min(take, notTake);
+        if(amount >= coins[i])
+        {
+            take = 1 + f(i, amount-coins[i], coins, dp);
+        }
+
+        return dp[i][amount] = min(take, notTake);
+
     }
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        //vector<vector<int>>dp(n, vector<int>(amount + 1, 0));
-        vector<int>prev(amount+1, 0), curr(amount+1, 0);
-
-        for(int i=0; i<=amount; i++)
-        {
-            if(i % coins[0] == 0)
-            {
-                prev[i] = i / coins[0];
-            }
-            else prev[i] = 1e9;
-        }
-
-        for(int i=1; i<n; i++)
-        {
-            for(int j=0; j<=amount; j++)
-            {
-                int notTake = prev[j];
-                int take = INT_MAX;
-                if(j >= coins[i]) take = 1 + curr[j - coins[i]];
-
-                curr[j] = min(take, notTake);
-            }
-            prev = curr;
-        }
-
-        if(prev[amount] >= 1e9) return -1;
-        return prev[amount];
-
-
-
-
-
-
-
-
-        // int ans =  f(n-1, amount, coins, dp);
-        // if(ans >= 1e9) return -1;
-        // return ans;
+        vector<vector<int>>dp(coins.size(), vector<int>(amount+1, -1));
+        int ans = f(coins.size()-1, amount, coins, dp);
+        if(ans >= 1e7) return -1;
+        return ans;
     }
 };
