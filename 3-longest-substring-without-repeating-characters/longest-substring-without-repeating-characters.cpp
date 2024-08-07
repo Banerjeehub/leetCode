f2485c71b032b85f90c6b3f40fb76b4e511cdd34
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-07 14:46:11 +0530

    Time: 20 ms (37.16%) | Memory: 14.8 MB (16.37%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(const string& s) {
-        unordered_set<char>
-            charSet;       // Set to store characters in the current window
-        int start = 0;     // Start index of the window
-        int maxLength = 0; // Maximum length of substring found
+    int lengthOfLongestSubstring(string s) {
+        
+        int start = 0;
+        unordered_set<char>st;
+        int maxLen = 0;
 
-        for (int end = 0; end < s.length(); ++end) {
-            // If the character is already in the set, remove characters from
-            // the start
-            while (charSet.find(s[end]) != charSet.end()) {
-                charSet.erase(s[start]);
-                ++start;
+        for(int end=0; end<s.length(); end++)
+        {
+            while(st.find(s[end]) != st.end())
+            {
+                st.erase(s[start]);
+                start++;
             }
-
-            // Add the current character to the set
-            charSet.insert(s[end]);
-            // Update the maximum length
-            maxLength = max(maxLength, end - start + 1);
+            st.insert(s[end]);
+            maxLen = max(maxLen, end - start + 1);
         }
 
-        return maxLength;
+        return maxLen;
     }
 };
