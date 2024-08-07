0fd580da584f03339ec21cd044540a8c34de293d
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-07 18:15:57 +0530

    Time: 4 ms (55.99%) | Memory: 14.5 MB (75.31%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-
+        if(nums.size() == 1) return;
+        if(nums.size() == 2)
+        {
+            swap(nums[0], nums[1]);
+            return;
+        }
         int n = nums.size();
-        int i = n - 2;
-
-        // Step 1: Find the largest index `i` such that `nums[i] < nums[i + 1]`
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        // Step 2: If no such index exists, the permutation is the last
-        // permutation
-        if (i >= 0) {
-            // Step 3: Find the largest index `j` such that `nums[j] > nums[i]`
-            int j = n - 1;
-            while (j >= 0 && nums[j] <= nums[i]) {
-                j--;
+        int idx = -1;
+        for(int i=n-2; i>=0; i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                idx = i;
+                break;
             }
-            // Step 4: Swap the values of `nums[i]` and `nums[j]`
-            swap(nums[i], nums[j]);
         }
 
-        // Step 5: Reverse the subarray from `i + 1` to the end
-        reverse(nums.begin() + i + 1, nums.end());
-        
+        if(idx == -1)
+        {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+
+        for (int i = n - 1; i > idx; i--) {
+            if (nums[i] > nums[idx]) {
+                swap(nums[i], nums[idx]);
+                break;
+            }
+        }
+
+        reverse(nums.begin()+idx+1, nums.end());
+        return;
     }
 };
