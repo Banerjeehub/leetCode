b07bd5b4c06614cd96891181e74ad41496d93d15
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-08 18:28:34 +0530

    Time: 1248 ms (68.61%) | Memory: 8.1 MB (54.84%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool isPalindrome(int i, int j, string &s)
-    {
-        while(i < j)
-        {
-            if(s[i] != s[j]) return false;
+    bool isPalindrome(int i, int j, string& s) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
             i++;
             j--;
         }
 
         return true;
     }
-    int helper(int idx, string &s, vector<int>&dp)
-    {
-        if(idx == s.size())
-        {
+    int helper(int idx, string& s, vector<int>& dp) {
+        if (idx == s.size()) {
             return 0;
         }
-        if(dp[idx] != -1) return dp[idx];
+        if (dp[idx] != -1)
+            return dp[idx];
         int minCost = INT_MAX;
-        for(int i=idx; i<s.size(); i++)
-        {
-            if(isPalindrome(idx, i, s))
-            {
-                int cost = 1 + helper(i+1 , s, dp);
+        for (int i = idx; i < s.size(); i++) {
+            if (isPalindrome(idx, i, s)) {
+                int cost = 1 + helper(i + 1, s, dp);
                 minCost = min(cost, minCost);
             }
         }
@@ -31,7 +27,7 @@ public:
         return dp[idx] = minCost;
     }
     int minCut(string s) {
-        vector<int>dp(s.length(), -1);
-        return helper(0,s,dp) - 1;
+        vector<int> dp(s.length(), -1);
+        return helper(0, s, dp) - 1;
     }
 };
