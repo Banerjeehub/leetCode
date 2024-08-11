72356b8184337bd9404c1a4bace00f57ced515fb
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-12 00:25:02 +0530

    Time: 158 ms (50.13%) | Memory: 79.1 MB (46.93%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-long long countGood(vector<int>& nums, int k) {
+    long long countGood(vector<int>& nums, int k) {
+        
         long long count = 0;
-        long long pairs = 0;
-        unordered_map<int, int> freqMap;
+        long long pair = 0;
+        unordered_map<int, int>mpp;
+
         int left = 0;
-
-        for (int right = 0; right < nums.size(); ++right) {
+        for(int right = 0; right < nums.size(); right++)
+        {
             int num = nums[right];
-            // Update the number of pairs with the current element
-            if (freqMap[num] > 0) {
-                pairs += freqMap[num];
+            if(mpp[num] > 0)
+            {
+                pair += mpp[num];
             }
-            freqMap[num]++;
+            mpp[num]++;
 
-            // While the number of pairs is at least k, count the number of subarrays
-            while (pairs >= k) {
-                count += (nums.size() - right); // Count subarrays from left to the end
+            while(pair >= k)
+            {
+                count += nums.size() - right;
                 int leftNum = nums[left];
-                // Update the number of pairs with the element being removed
-                if (freqMap[leftNum] > 1) {
-                    pairs -= (freqMap[leftNum] - 1);
+                if(mpp[leftNum] > 1)
+                {
+                    pair -= (mpp[leftNum] - 1);
                 }
-                freqMap[leftNum]--;
+                mpp[leftNum]--;
                 left++;
             }
         }
