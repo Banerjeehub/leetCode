29bedf75bee3c934f193351c2a5c9f1d06ca1afe
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-13 22:44:52 +0530

    Time: 4 ms (31.97%) | Memory: 10 MB (52.89%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans;
+vector<int>ans;
         TreeNode* curr = root;
-        
-        while (curr) {
-            if (!curr->left) {
-                // Process the current node (preorder visit)
-                ans.push_back(curr->val);
-                // Move to the right subtree
-                curr = curr->right;
-            } else {
-                TreeNode* prev = curr->left;
 
-                // Find the rightmost node in the left subtree or the thread
-                while (prev->right && prev->right != curr) {
+        while(curr)
+        {
+            if(curr->left == NULL)
+            {
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else
+            {
+                TreeNode* prev = curr->left;
+                while(prev->right && prev->right != curr)
+                {
                     prev = prev->right;
                 }
 
-                // If the right pointer is null, make the current node the right child
-                if (!prev->right) {
+                if(prev->right == NULL)
+                {
                     prev->right = curr;
-                    // Process the current node (preorder visit)
                     ans.push_back(curr->val);
-                    // Move to the left subtree
                     curr = curr->left;
-                } else {
-                    // Restore the tree structure
+                }
+                else
+                {
+                    
                     prev->right = NULL;
-                    // Move to the right subtree
                     curr = curr->right;
                 }
             }
         }
-        
         return ans;
     }
 };
