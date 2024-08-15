29f6d202ef6bea08493c5726a4a5436d9461e48e
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-16 00:37:46 +0530

    Time: 19 ms (80.30%) | Memory: 30.2 MB (5.05%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
-    void getLcs(string s1, string s2, int n, int m, vector<vector<int>>&dp)
+    void lcs(int n, int m, string &s1, string &s2, vector<vector<int>>&dp)
     {
-        for(int i=0; i<=m; i++) dp[0][i] = 0;
-        for(int i=0; i<=n; i++) dp[i][0] = 0;
-
         for(int i=1; i<=n; i++)
         {
             for(int j=1; j<=m; j++)
@@ -13,42 +10,33 @@ public:
                 {
                     dp[i][j] = 1 + dp[i-1][j-1];
                 }
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
             }
         }
-    }
-    string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
-        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
-        getLcs(str1, str2, n, m, dp);
 
-        int i = n;
-        int j = m;
+        
+    }
+    string printSCS(int i, int j, string &str1, string &str2, vector<vector<int>>&dp) {
+        
         string ans = "";
-        while(i > 0 && j > 0)
-        {
-            if(str1[i-1] == str2[j-1])
-            {
-                ans = str1[i-1] + ans;
-                i--, j--;
-            }
-            else
-            {
-                if(dp[i-1][j] > dp[i][j-1])
-                {
-                    ans = str1[i-1] + ans;
+        while (i > 0 && j > 0) {
+            if (str1[i - 1] == str2[j - 1]) {
+                ans = str1[i - 1] + ans;
+                i--;
+                j--;
+            } else {
+                if (dp[i - 1][j] > dp[i][j - 1]) {
+                    ans = str1[i - 1] + ans;
                     i--;
-                }
-                else 
-                {
-                    ans = str2[j-1] + ans;
+                } else {
+                    ans = str2[j - 1] + ans;
                     j--;
                 }
- 
             }
         }
-
         while(i > 0)
         {
             ans = str1[i-1] + ans;
@@ -61,6 +49,13 @@ public:
         }
 
         return ans;
-
+    }
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size();
+        int m = str2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        lcs(n, m, str1, str2, dp);
+        string ans = printSCS(n, m, str1, str2, dp);
+        return ans;
     }
 };
