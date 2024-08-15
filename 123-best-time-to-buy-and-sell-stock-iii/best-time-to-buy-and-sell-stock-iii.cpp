e08585593accd9722026aa86d58a7934084560d1
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-15 21:03:53 +0530

    Time: 533 ms (8.52%) | Memory: 222.3 MB (24.82%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int f(int idx, int liberty, int cap, vector<int>&prices, vector<vector<vector<int>>>&dp)
+    int f(int idx, int lib, int cap, vector<int>&prices, vector<vector<vector<int>>>&dp)
     {
         if(idx == prices.size()) return 0;
-        if(cap == 0) return 0;
-        if(dp[idx][liberty][cap] != -1) return dp[idx][liberty][cap];
+        if(cap <= 0) return 0;
+
+        if(dp[idx][lib][cap] != -1) return dp[idx][lib][cap];
         int profit = 0;
-        if(liberty)
+        if(lib)
         {
-            profit = max(-prices[idx]+f(idx+1, 0, cap, prices, dp), f(idx+1, liberty, cap, prices, dp));
+            profit = max(-prices[idx]+ f(idx + 1, 0, cap, prices, dp), f(idx+1, 1, cap, prices, dp));
         }
         else
         {
-            profit = max(prices[idx]+f(idx+1, 1, cap-1, prices, dp), f(idx+1, liberty, cap, prices, dp));
+            profit = max(prices[idx] + f(idx+1, 1, cap-1, prices, dp), f(idx+1, 0, cap, prices, dp));
         }
 
-        return dp[idx][liberty][cap] = profit;
+        return dp[idx][lib][cap] = profit;
+
     }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         vector<vector<vector<int>>>dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-        return f(0, 1, 2, prices, dp);
+        return f(0, 1, 2, prices,dp);
     }
 };
