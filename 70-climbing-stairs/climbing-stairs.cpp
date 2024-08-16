0e6c1cf8882d8c419a313c19e02694e66b62408c
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-16 11:46:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (31.26%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -5,10 +5,11 @@ public:
         if(i < 0) return 0;
         if(i == 0) return 1;
         if(dp[i] != -1) return dp[i];
-        int one = f(i-1, dp);
-        int two = f(i-2, dp);
+        int oneStep = f(i-1, dp);
+        int twoStep = f(i-2, dp);
 
-        return dp[i] = one + two;
+        return dp[i] = oneStep + twoStep;
+
     }
     int climbStairs(int n) {
         vector<int>dp(n+1, -1);
