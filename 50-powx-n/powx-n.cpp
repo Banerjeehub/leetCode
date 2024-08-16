e3532a82639566f49de19def19d9af6437c2a564
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-16 13:54:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (75.31%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        
+    double f(double x, int n)
+    {
         if(n == 0) return 1;
         if(n < 0)
         {
             n = abs(n);
-            x = 1 / x;
+            x = 1/x;
         }
-
         if(n % 2)
         {
-            return x * myPow(x, n-1);
-        }else
-        {
-            return myPow(x*x, n/2);
+            return x * f(x, n-1);
         }
+        else
+        {
+            return f(x*x, n/2);
+        }
+    }
+    double myPow(double x, int n) {
+        
+        return f(x, n);
     }
 };
