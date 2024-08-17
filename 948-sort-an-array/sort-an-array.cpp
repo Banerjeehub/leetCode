81b347e1cc0eb5620abb4daf3c941e7d16516375
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-17 09:37:56 +0530

    Time: 613 ms (15.43%) | Memory: 284.3 MB (13.95%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,8 +1,46 @@
 class Solution {
 public:
+    void ms(int s, int mid, int e, vector<int>&nums)
+    {
+        int left = s;
+        int right = mid + 1;
+        vector<int>temp;
+
+        while(left <= mid && right <= e)
+        {
+            if(nums[left] < nums[right])
+            {
+                temp.push_back(nums[left++]);
+            }
+            else temp.push_back(nums[right++]);
+        }
+        while(left <= mid)
+        {
+            temp.push_back(nums[left++]);
+        }
+        while(right <= e)
+        {
+            temp.push_back(nums[right++]);
+        }
+
+        for(int i=s; i<=e; i++)
+        {
+            nums[i] = temp[i-s];
+        }
+    }
+    void merge(int s, int e, vector<int>&nums)
+    {
+        if(s >= e) return;
+        int mid = (s + e)/2;
+
+        merge(s, mid, nums);
+        merge(mid+1, e, nums);
+
+        ms(s, mid, e, nums);
+    }
     vector<int> sortArray(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());
+        merge(0, nums.size()-1, nums);
         return nums;
     }
 };
