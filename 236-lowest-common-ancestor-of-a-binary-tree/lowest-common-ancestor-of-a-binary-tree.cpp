b4c792e3f10ccbfa395d6731c1d08ae65dd83b03
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-17 18:31:51 +0530

    Time: 28 ms (6.77%) | Memory: 20.9 MB (6.45%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,42 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // Stack for tree traversal
-        stack<TreeNode*> nodeStack;
-        // Map to keep track of parent pointers for each node
-        unordered_map<TreeNode*, TreeNode*> parent;
-        // Initialize the parent of the root as NULL
+        unordered_map<TreeNode*, TreeNode*>parent;
+        queue<TreeNode*>qu;
+        qu.push(root);
         parent[root] = nullptr;
-        nodeStack.push(root);
 
-        // Iterate until both nodes are found
-        while (parent.find(p) == parent.end() || parent.find(q) == parent.end()) {
-            TreeNode* node = nodeStack.top();
-            nodeStack.pop();
+        while(parent.find(p) == parent.end() || parent.find(q) == parent.end())
+        {
+            auto top = qu.front();
+            qu.pop();
 
-            // If the current node has a left child, process it
-            if (node->left) {
-                parent[node->left] = node;
-                nodeStack.push(node->left);
+            if(top->left)
+            {
+                parent[top->left] = top;
+                qu.push(top->left);
             }
-            // If the current node has a right child, process it
-            if (node->right) {
-                parent[node->right] = node;
-                nodeStack.push(node->right);
+            if(top->right)
+            {
+                parent[top->right] = top;
+                qu.push(top->right);
             }
+
         }
 
-        // Set to store the ancestors of p
-        unordered_set<TreeNode*> ancestors;
-
-        // Process all ancestors for node p using parent pointers
-        while (p) {
-            ancestors.insert(p);
+        set<TreeNode*>st;
+        while(p)
+        {
+            st.insert(p);
             p = parent[p];
         }
 
-        // The first ancestor of q which appears in p's ancestor set is the LCA
-        while (ancestors.find(q) == ancestors.end()) {
+        while(st.find(q) == st.end())
+        {
             q = parent[q];
         }
 
