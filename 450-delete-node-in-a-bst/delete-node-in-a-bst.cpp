b766ced62587a24e7fb7ce75e0dc71b009d4d72d
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-18 20:34:28 +0530

    Time: 29 ms (23.76%) | Memory: 33.6 MB (16.16%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -12,46 +12,46 @@
  */
 class Solution {
 public:
-    TreeNode* helper(TreeNode* root) {
-        if (!root->left)
-            return root->right;
-        if (!root->right)
-            return root->left;
+    TreeNode* helper(TreeNode* root)
+    {
+        if(!root->left) return root->right;
+        if(!root->right) return root->left;
 
         TreeNode* rightChild = root->right;
         TreeNode* lastNode = rightMostNode(root->left);
         lastNode->right = rightChild;
         return root->left;
+
     }
-    TreeNode* rightMostNode(TreeNode* root) {
-        if (!root->right)
-            return root;
+    TreeNode* rightMostNode(TreeNode* root)
+    {
+        if(!root->right) return root;
         return rightMostNode(root->right);
     }
     TreeNode* deleteNode(TreeNode* root, int key) {
-
         if (!root)
             return NULL;
-
         if (root->val == key) {
             return helper(root);
         }
+
         TreeNode* temp = root;
         while (root) {
             if (root->val > key) {
                 if (root->left && root->left->val == key) {
                     root->left = helper(root->left);
                     break;
-                } else {
+                } else
                     root = root->left;
-                }
-            } else {
-                if (root->right && root->right->val == key) {
+            }
+            else
+            {
+                if(root->right && root->right->val == key)
+                {
                     root->right = helper(root->right);
                     break;
-                } else {
-                    root = root->right;
                 }
+                else root = root->right;
             }
         }
 
