0fc09831c422d0543b3136b230ee6f74e1c11db9
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-20 01:19:03 +0530

    Time: 20 ms (82.59%) | Memory: 33.5 MB (16.98%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -1,60 +1,54 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
+
 class Solution {
 public:
     TreeNode* helper(TreeNode* root)
     {
-        if(!root->left) return root->right;
         if(!root->right) return root->left;
+        if(!root->left) return root->right;
 
         TreeNode* rightChild = root->right;
-        TreeNode* lastNode = rightMostNode(root->left);
-        lastNode->right = rightChild;
+        TreeNode* left = rightMostNode(root->left);
+        left->right = rightChild;
         return root->left;
-
     }
     TreeNode* rightMostNode(TreeNode* root)
     {
         if(!root->right) return root;
         return rightMostNode(root->right);
     }
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if (!root)
-            return NULL;
-        if (root->val == key) {
+    TreeNode* deleteNode(TreeNode* root, int k) {
+        
+        if(!root) return nullptr;
+
+        if(root->val == k)
+        {
             return helper(root);
         }
-
-        TreeNode* temp = root;
-        while (root) {
-            if (root->val > key) {
-                if (root->left && root->left->val == key) {
+        TreeNode* curr = root;
+        while(root)
+        {
+            if(root->val > k)
+            {
+                if(root->left && root->left->val == k)
+                {
                     root->left = helper(root->left);
                     break;
-                } else
-                    root = root->left;
+                }
+                else root = root->left;
             }
             else
             {
-                if(root->right && root->right->val == key)
+                if(root->right && root->right->val == k)
                 {
                     root->right = helper(root->right);
                     break;
+
                 }
                 else root = root->right;
             }
         }
 
-        return temp;
+        return curr;
+
     }
 };
