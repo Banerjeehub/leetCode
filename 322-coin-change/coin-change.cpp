96fcd9874e2c0922a68cd516a4db3de21c29eb64
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-19 18:18:21 +0530

    Time: 44 ms (39.91%) | Memory: 47.7 MB (25.95%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int f(int idx, vector<int>& coins, int amount, vector<vector<int>>&dp)
+    int f(int idx, int amount, vector<int>&coins, vector<vector<int>>&dp)
     {
         if(idx == 0)
         {
             if(amount % coins[0] == 0) return amount / coins[0];
             return 1e7;
         }
-        if(dp[idx][amount] != -1) return dp[idx][amount];
 
-        int notTake = f(idx-1, coins, amount, dp);
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+        int notTake = f(idx-1, amount, coins, dp);
         int take = 1e7;
-        if(amount >= coins[idx])
+        if(amount >= coins[idx]) 
         {
-            take = 1 + f(idx, coins, amount - coins[idx], dp);
+            take = 1 + f(idx, amount - coins[idx], coins, dp);
         }
 
         return dp[idx][amount] = min(take, notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(coins.size(), vector<int>(amount + 1, -1));
-        int ans = f(n-1, coins, amount, dp);
-        if(ans == 1e7)
-        {
-            return -1;
-        }
-        return ans;
+       
+
+        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
+        int ans = f(n-1, amount, coins, dp);
+        if(ans != 1e7) return ans;
+        return -1;
     }
 };
