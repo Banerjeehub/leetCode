21cd79c3487e4ab042cd05b745a683f745ed8ef2
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-24 12:04:33 +0530

    Time: 2 ms (50.23%) | Memory: 9.8 MB (9.62%) - LeetSync

--- 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
+++ 564-find-the-closest-palindrome/find-the-closest-palindrome.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
     string nearestPalindromic(string n) {
-
         int len = n.size();
-        set<string> candidates;
+        set<string>candidates;
 
-        candidates.insert(string(len - 1, '9'));
-        candidates.insert("1" + string(len - 1, '0') + "1");
+        candidates.insert(string(len-1, '9'));
+        candidates.insert("1" + string(len-1, '0') + "1");
 
-        long prefix = 0;
-
-        try {
-            prefix = stol(n.substr(0, (len + 1) / 2));
-        } catch (const invalid_argument& e) {
+        long prefixNum = 0;
+        try{
+            prefixNum = stol(n.substr(0, (len+1)/2));
+        }catch(const invalid_argument& e)
+        {
             return "";
         }
 
-        // generate the possible answers
-
-        for (int i = -1; i <= 1; i++) {
-            string str = to_string(prefix + i);
+        for(int i=-1; i<=1; i++)
+        {
+            string prefix = to_string(prefixNum + i);
             string candidate;
 
-            if (len % 2 == 0) {
-                candidate = str + string(str.rbegin(), str.rend());
-            } else {
-                candidate = str + string(str.rbegin() + 1, str.rend());
+            if(len % 2 == 0)
+            {
+                candidate = prefix + string(prefix.rbegin(), prefix.rend());
             }
+            else candidate = prefix + string(prefix.rbegin()+1, prefix.rend());
 
             candidates.insert(candidate);
         }
 
         candidates.erase(n);
 
+
         string ans;
         long min_diff = LONG_MAX;
 
-        for (auto it : candidates) {
+        for(auto it : candidates)
+        {
             long diff = 0;
-            try {
-
-                diff = abs(stol(it) - stol(n));
-            } catch (const invalid_argument& e) {
+            try{
+                diff = abs(stol(n) - stol(it));
+            }
+            catch(const invalid_argument& e)
+            {
                 continue;
             }
 
-            if (diff < min_diff || (diff == min_diff && stol(it) < stol(ans))) {
-                min_diff = diff;
+
+            if(diff < min_diff || (diff == min_diff && stol(it) < stol(ans)))
+            {
                 ans = it;
+                min_diff = diff;
             }
         }
 
