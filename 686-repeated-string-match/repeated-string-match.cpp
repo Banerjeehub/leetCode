908a76f1706bda338fec289ae5c4cecb8b3f808f
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2024-08-24 19:26:59 +0530

    Time: 10 ms (39.13%) | Memory: 8.5 MB (72.86%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        int minReps = (b.size() + a.size() - 1) / a.size(); // Ceiling of b.size() / a.size()
+        
+        // Create the repeated string with minReps and minReps + 1
+        string repeatedA = a;
+        for (int i = 1; i < minReps; ++i) {
+            repeatedA += a;
+        }
+        
+        // Check if b is a substring of the repeated string
+        if (repeatedA.find(b) != string::npos) {
+            return minReps;
+        }
+        
+        // Add one more repetition and check again
+        repeatedA += a;
+        if (repeatedA.find(b) != string::npos) {
+            return minReps + 1;
+        }
+        
+        // If not found in either case, return -1
+        return -1;
+    }
+};
