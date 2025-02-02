4e757db21745ea62b0145ee9a55e1aa85668dcc9
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-02-02 19:10:24 +0530

    Time: 0 ms (100.00%) | Memory: 113.8 MB (45.98%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -1,22 +1,23 @@
-
-using namespace std;
-
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-
-        double sum = 0;
-        double maxAverage = -numeric_limits<double>::infinity();
-        for (int i = 0; i < k; i++) {
-            sum += nums[i];
+        
+        double iniAvg = 0;
+        for(int i=0; i<k; i++)
+        {
+            iniAvg += nums[i];
+        }
+        
+        double maxAvg = iniAvg/k;
+        
+        for(int i=k; i<nums.size(); i++)
+        {
+            iniAvg += (nums[i] - nums[i-k]);
+            maxAvg = max(iniAvg/k, maxAvg);
         }
 
-        maxAverage = max(maxAverage, sum / k);
-        for (int i = k; i < nums.size(); i++) {
-            sum += nums[i] - nums[i - k]; 
-            maxAverage = max(maxAverage, sum / k);  
-        }
+        return maxAvg;
 
-        return maxAverage;
+        
     }
 };
