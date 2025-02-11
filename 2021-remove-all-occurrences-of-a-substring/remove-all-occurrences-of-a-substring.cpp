9283d9b2a83aced23d0862c00ef35ac53f783b6e
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-02-11 12:17:24 +0530

    Time: 519 ms (5.85%) | Memory: 422.9 MB (5.32%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-
-         string result;
-    stack<char> st;
-    
-    for (char c : s) {
-        st.push(c);
         
-        // Check if the top of the stack forms `part`
-        if (st.size() >= part.size()) {
-            string temp;
-            for (int i = part.size() - 1; i >= 0; --i) {
-                temp = st.top() + temp;
-                st.pop();
-            }
-            
-            // If temp is not equal to `part`, push back the characters
-            if (temp != part) {
-                for (char ch : temp) {
-                    st.push(ch);
+        string res;
+        stack<char>st;
+
+        for(auto c : s)
+        {
+            st.push(c);
+
+            if(st.size() >= part.size())
+            {
+                string temp = "";
+                for(int i=0; i<part.size(); i++)
+                {
+                    temp = st.top() + temp;
+                    st.pop();
+                }
+
+                if(temp != part)
+                {
+                    for(auto ch : temp)
+                    {
+                        st.push(ch);
+                    }
                 }
             }
         }
-    }
-    
-    // Construct the final string
-    while (!st.empty()) {
-        result = st.top() + result;
-        st.pop();
-    }
-    
-    return result;
+
+        while(!st.empty())
+        {
+            res = st.top() + res;
+            st.pop();
+        }
+
+        return res;
     }
 };
