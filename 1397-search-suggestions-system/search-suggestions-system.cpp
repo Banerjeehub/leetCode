06e74207c6a0a2bcba6df639d78f36b4ec754e91
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-05 12:27:20 +0530

    Time: 460 ms (18.45%) | Memory: 187.3 MB (18.41%) - LeetSync

---
+++ 1397-search-suggestions-system/search-suggestions-system.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        
+        sort(products.begin(), products.end());
+        string prefix = "";
+        vector<vector<string>>ans;
+
+        for(auto ch : searchWord)
+        {
+            vector<string>temp;
+            prefix += ch;
+            for(auto product : products)
+            {
+                if(product.find(prefix) == 0)
+                {
+                    temp.push_back(product);
+                    if(temp.size() == 3)
+                    {
+                        break;
+                    }
+                }
+            }
+            ans.push_back(temp);
+
+        }
+
+        return ans;
+    }
+};
