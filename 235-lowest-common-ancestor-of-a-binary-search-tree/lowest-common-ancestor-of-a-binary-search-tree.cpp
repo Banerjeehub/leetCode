38c32a0df86302b44bd0164fa27d096b36fdc97b
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-09 13:13:17 +0530

    Time: 22 ms (61.37%) | Memory: 23.4 MB (89.96%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,23 +11,25 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        TreeNode* current = root;
         
-        while (current) {
-            if (p->val < current->val && q->val < current->val) {
-                // Both nodes are in the left subtree
-                current = current->left;
-            } 
-            else if (p->val > current->val && q->val > current->val) {
-                // Both nodes are in the right subtree
-                current = current->right;
-            } 
-            else {
-                // We have found the split point, i.e., the LCA node
-                return current;
+        TreeNode* curr = root;
+
+        while(curr != NULL)
+        {
+            if(p->val < curr->val && q->val < curr->val)
+            {
+                curr = curr->left;
+            }
+            else if(p->val > curr->val && q->val > curr->val)
+            {
+                curr = curr -> right;
+            }
+            else
+            {
+                return curr;
             }
         }
-        
-        return nullptr; // This line should not be reached if p and q are guaranteed to be in the tree
+
+        return nullptr;
     }
 };
