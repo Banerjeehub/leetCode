a035930c0709aed57663a0527eb88365673d3b50
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-09 17:07:06 +0530

    Time: 14 ms (99.30%) | Memory: 17.5 MB (48.60%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    int dfs(int i, int j, int n, int m, vector<vector<char>>& grid,
-            vector<vector<int>>& vis) {
+    int dfs(int row, int col, int n, int m,vector<vector<int>>& visited, vector<vector<char>>& grid)
+    {
+        if(row < 0 || col < 0 || row >= n || col >= m || visited[row][col] || grid[row][col] != '1') return 0;
 
-        if (i < 0 || j < 0 || i >= n || j >= m || vis[i][j] ||
-            grid[i][j] != '1')
-            return 0;
-        vis[i][j] = 1;
+        visited[row][col] = 1;
         int dx[] = {-1, 1, 0, 0};
         int dy[] = {0, 0, -1, 1};
 
-       
-            for (int dj = 0; dj <4; dj++) {
-                int nr = i + dx[dj];
-                int nc = j + dy[dj];
-                dfs(nr, nc, n, m, grid, vis);
-            }
-        
+        for(int x=0; x<4; x++)
+        {
+            int nr = dx[x] + row;
+            int nc = dy[x] + col;
+            dfs(nr, nc, n, m, visited, grid);
+        }
 
         return 1;
+
     }
     int numIslands(vector<vector<char>>& grid) {
+        int count = 0;
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (!vis[i][j] && grid[i][j] == '1') {
-                    count += dfs(i, j, n, m, grid, vis);
+        vector<vector<int>>visited(grid.size(), vector<int>(grid[0].size(), 0));
+        for(int i=0; i<grid.size(); i++)
+        {
+            for(int j=0; j<grid[0].size(); j++)
+            {
+                if(!visited[i][j] && grid[i][j] == '1')
+                {
+                    count += dfs(i, j, n, m, visited, grid);
                 }
             }
         }
