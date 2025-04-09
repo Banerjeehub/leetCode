7c844c3c72b3868c92528ce11ecc3f6f919fc147
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-09 16:00:39 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (19.59%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,19 +1,44 @@
+// class Solution {
+// public:
+//     long prev = LONG_MIN;
+
+//     bool inorder(TreeNode* root) {
+//         if (!root) return true;
+
+//         if (!inorder(root->left)) return false;
+
+//         if (root->val <= prev) return false;
+//         prev = root->val;
+
+//         return inorder(root->right);
+//     }
+
+//     bool isValidBST(TreeNode* root) {
+//         return inorder(root);
+//     }
+// };
+
+
 class Solution {
 public:
-    long prev = LONG_MIN;
-
-    bool inorder(TreeNode* root) {
-        if (!root) return true;
-
-        if (!inorder(root->left)) return false;
-
-        if (root->val <= prev) return false;
-        prev = root->val;
-
-        return inorder(root->right);
-    }
-
     bool isValidBST(TreeNode* root) {
-        return inorder(root);
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        long prev = LONG_MIN;
+
+        while (curr || !st.empty()) {
+            while (curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            curr = st.top(); st.pop();
+            if (curr->val <= prev) return false;
+            prev = curr->val;
+
+            curr = curr->right;
+        }
+
+        return true;
     }
 };
