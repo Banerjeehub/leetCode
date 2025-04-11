172ebfdb3a0d24f546856fb79618418e76aecfe8
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-11 10:45:39 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (72.17%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& as) {
-        int n = as.size();
-        stack<int>st;
 
-        for(int i=0; i<n; i++)
-        {
-            if(st.empty() || as[i] > 0)
-            {
+        // smaller one will explode (if moving in the opposite direction)
+        // same direction will never meet
+
+        stack<int> st;
+        for (int i = 0; i < as.size(); i++) {
+            if (st.empty() || as[i] > 0) {
                 st.push(as[i]);
-            }
-            else
-            {
-                while(!st.empty() && st.top() > 0 && st.top() < abs(as[i]))
-                {
+            } else {
+                while (!st.empty() && st.top() > 0 && abs(as[i]) > st.top()) {
                     st.pop();
                 }
-                if(!st.empty() && st.top() == abs(as[i])) st.pop();
-                else
-                {
-                    if(st.empty() || st.top() < 0)
-                    {
-                        st.push(as[i]);
-                    }
+                if (!st.empty() && st.top() == abs(as[i])) {
+                    st.pop();
+                } else {
+                    if(st.empty() || st.top() < 0) st.push(as[i]);
                 }
             }
         }
 
-        vector<int>ans;
-        while(!st.empty())
-        {
+        vector<int> ans;
+        while (!st.empty()) {
             ans.push_back(st.top());
             st.pop();
         }
+
         reverse(ans.begin(), ans.end());
         return ans;
     }
