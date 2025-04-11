8ebdeb173ed560e8384ef56baaa6d55406d385e5
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-11 23:38:40 +0530

    Time: 8 ms (86.64%) | Memory: 88.7 MB (56.53%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
-    int n = heights.size();
-    vector<int> ans(n, 0);
-    stack<int> st; // stack stores indices or heights
+        
+        vector<int>ans(heights.size(), 0);
+        stack<int>st;
 
-    for (int i = n - 1; i >= 0; --i) {
-        int count = 0;
+        for(int i=heights.size()-1; i>=0; i--)
+        {
+            int count = 0;
+            while(!st.empty() && heights[i] > st.top())
+            {
+                st.pop();
+                count++;
+            }
+            if(!st.empty())
+            {
+                count++;
+            }
 
-        // Pop all shorter people (visible)
-        while (!st.empty() && heights[i] > st.top()) {
-            st.pop();
-            count++;
+            ans[i] = count;
+            st.push(heights[i]);
         }
 
-        // If someone taller or equal is still there, they can also be seen
-        if (!st.empty()) count++;
-
-        ans[i] = count;
-        st.push(heights[i]);
+        return ans;
     }
-
-    return ans;
-}
 };
