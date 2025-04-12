75d38ea55f0edc6daa8141b522ab3c8090a2c663
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-12 08:41:27 +0530

    Time: 15 ms (55.77%) | Memory: 88.7 MB (26.86%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& heights) {
-        
-        vector<int>ans(heights.size(), 0);
+        int n = heights.size();
+        vector<int>ans(n, 0);
         stack<int>st;
 
-        for(int i=heights.size()-1; i>=0; i--)
+        for(int i=n-1; i>=0; i--)
         {
             int count = 0;
             while(!st.empty() && heights[i] > st.top())
