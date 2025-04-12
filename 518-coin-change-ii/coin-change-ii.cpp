53b2abe633c8b0d7b4b9e244820cf61a7b942969
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-12 10:01:17 +0530

    Time: 15 ms (75.24%) | Memory: 28.3 MB (62.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
-    int f(int idx, int amount, vector<int>&coins, vector<vector<int>>&dp)
+    int helper(int amount, int idx, vector<int>&coins, vector<vector<int>>&dp)
     {
         if(idx == 0)
         {
-            return (amount % coins[0] == 0);
+            return (amount % coins[idx] == 0) ? 1 : 0;
+            
         }
-
         if(dp[idx][amount] != -1) return dp[idx][amount];
-
-        int notTake = f(idx-1, amount, coins, dp);
+        int notTake = helper(amount, idx-1, coins, dp);
         int take = 0;
-        if(amount >= coins[idx]) take = f(idx, amount-coins[idx], coins, dp);
+
+        if(amount >= coins[idx])
+        {
+            take = helper(amount - coins[idx], idx, coins, dp);
+        }
 
         return dp[idx][amount] = take + notTake;
     }
     int change(int amount, vector<int>& coins) {
-        int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, 0));
-        //return f(n-1, amount, coins, dp);
-        vector<int>prev(amount+1, 0), curr(amount+1, 0);
-
-        for(int i=0; i<=amount; i++)
-        {
-            if(i % coins[0] == 0) prev[i] = 1;  
-        }
-
-        for(int i=1; i<n; i++)
-        {
-            for(int j=0; j<=amount; j++)
-            {
-                int notTake = prev[j];
-                int take = 0;
-                if(j >= coins[i]) take = curr[j-coins[i]];
-
-                curr[j] = take + notTake;
-            }
-            prev = curr;
-        }
-
-        return prev[amount];
+        
+        vector<vector<int>>dp(coins.size(), vector<int>(amount+1, -1));
+        return helper(amount, coins.size()-1, coins, dp);
     }
 };
