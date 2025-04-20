398dbd3b3e93d65ac7b1722d69c168e728a87958
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-21 01:54:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.74%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -4,18 +4,22 @@ public:
         
         int totalRabbits = 0;
         unordered_map<int, int>freq;
-        for(auto it : answers)
+        for(auto it : answers) // T.C => o(n = answers.size())
         {
             freq[it]++; // how many rabbits have the same count will save it
+        
         }
 
-        for(auto it : freq)
+        for(auto it : freq) // o(m = freq.size() or number of distinct answers)
         {
             int groupSize = it.first + 1; // total count of the same colour including this one
             int totalGroups = ceil(double(it.second) / groupSize); // how many groups needed to seperate the total counts
             totalRabbits += groupSize * totalGroups;
 
         }
+
+        // total T.C => o(n + m)
+        // S.C => o(m)
 
         return totalRabbits;
     }
