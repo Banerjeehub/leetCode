96d1c7a9a9aaaf63a2b7585fe9e37eb2026f04b2
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-21 01:50:45 +0530

    Time: 4 ms (9.45%) | Memory: 12.2 MB (42.28%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.cpp
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
+        
         int totalRabbits = 0;
-        unordered_map<int, int> freq;
-
-        for (auto it : answers) {
-            freq[it]++;
+        unordered_map<int, int>freq;
+        for(auto it : answers)
+        {
+            freq[it]++; // how many rabbits have the same count will save it
         }
 
-        for (auto it : freq) {
-            int groupSize = it.first + 1;
-            int totalGroups = ceil((double)it.second / groupSize);
-            totalRabbits += totalGroups * groupSize;
+        for(auto it : freq)
+        {
+            int groupSize = it.first + 1; // total count of the same colour including this one
+            int totalGroups = ceil(double(it.second) / groupSize); // how many groups needed to seperate the total counts
+            totalRabbits += groupSize * totalGroups;
+
         }
 
         return totalRabbits;
