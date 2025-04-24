706c51bf6a92abe1f60710c33f0d6915b49b9f0e
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-04-25 01:21:37 +0530

    Time: 36 ms (27.24%) | Memory: 44.4 MB (34.05%) - LeetSync

--- 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int countCompleteSubarrays(vector<int>& nums) {
-    int n = nums.size();
-    unordered_set<int> totalSet(nums.begin(), nums.end());
-    int totalDistinct = totalSet.size();
-
-    int count = 0;
-
-    for (int start = 0; start < n; ++start) {
+    int atMostKDistinct(vector<int>& nums, int k) {
+        int n = nums.size();
         unordered_map<int, int> freq;
-        int distinct = 0;
+        int left = 0, count = 0;
 
-        for (int end = start; end < n; ++end) {
-            if (freq[nums[end]] == 0)
-                distinct++;
-            freq[nums[end]]++;
+        for (int right = 0; right < n; ++right) {
+            if (freq[nums[right]] == 0)
+                k--;
+            freq[nums[right]]++;
 
-            if (distinct == totalDistinct)
-                count++;
+            while (k < 0) {
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0)
+                    k++;
+                left++;
+            }
+
+            count += (right - left + 1);
         }
+
+        return count;
     }
 
-    return count;
-}
+    int countCompleteSubarrays(vector<int>& nums) {
+        unordered_set<int> totalSet(nums.begin(), nums.end());
+        int totalDistinct = totalSet.size();
+
+        return atMostKDistinct(nums, totalDistinct) -
+               atMostKDistinct(nums, totalDistinct - 1);
+    }
 };
