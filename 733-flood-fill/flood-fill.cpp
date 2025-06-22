d5d548c141ce845f759f33a15219777937e17c7d
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-06-22 23:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (61.86%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n = image.size();
-        int m = image[0].size();
-        vector<vector<int>>visited(n, vector<int>(m, 0));
-        queue<pair<int, int>>q;
-        q.push({sr, sc});
-        int initColor = image[sr][sc];
+    void dfs(vector<vector<int>>& image, vector<vector<int>>& visited, int sr, int sc, int color, int initColor, int n, int m)
+    {
+        visited[sr][sc] = 1;
         image[sr][sc] = color;
-        while(!q.empty())
+
+        int dr[] = {0,0,1,-1};
+        int dc[] = {1,-1,0,0};
+
+        for(int i=0; i<4; i++)
         {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-            int dr[] = {1, -1, 0, 0};
-            int dc[] = {0, 0, 1, -1};
-
-            for(int i=0; i<4; i++)
+            int nr = sr + dr[i];
+            int nc = sc + dc[i];
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m && !visited[nr][nc] && image[nr][nc] == initColor)
             {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
-
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m && !visited[nr][nc] && image[nr][nc] == initColor)
-                {
-                    q.push({nr, nc});
-                    visited[nr][nc] = 1;
-                    image[nr][nc] = color;
-                }
+                dfs(image, visited, nr, nc, color, initColor, n, m);
             }
         }
-
-        return image;
-
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
+        int n = image.size();
+        int m = image[0].size();
+
+        vector<vector<int>>visited(n, vector<int>(m, 0));
+        dfs(image, visited, sr, sc, color, image[sr][sc], n, m);
+        return image;
     }
 };
