fdd15c951ab3b0f52572f85eb7ada8230224d834
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2025-06-22 13:36:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (78.48%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
+    int sumOfDigit(int num) {
+        if(num == 0) return num;
+        return num % 10 + sumOfDigit(num/10);
+        
+    }
     int addDigits(int num) {
         // pre-order recurssion
 
         if (num < 10)
             return num;
-
-        int sum = 0;
-        while (num > 0) {
-            int mod = num % 10;
-            sum += mod;
-            num /= 10;
-        }
-
-        return addDigits(sum);
+        return addDigits(sumOfDigit(num));
     }
 };
