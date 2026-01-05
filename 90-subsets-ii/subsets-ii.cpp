8dff15bdc058eee985e1dccfd3ffb8c6f5bf8151
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2026-01-06 01:04:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (96.31%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
+    void helper(int idx, vector<int>&temp, vector<int>&nums, vector<vector<int>>&ans){
+        if(idx == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        // -------TAKE--------------
+        temp.push_back(nums[idx]);
+        helper(idx+1, temp, nums, ans);
+        temp.pop_back();
+
+        // -------NotTake-----------
+
+        while(idx+1 < nums.size() && nums[idx] == nums[idx+1]) idx++;
+        helper(idx+1, temp, nums, ans);
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
-        set<vector<int>>subs;
-        vector<vector<int>>ans;
         sort(nums.begin(), nums.end());
-        for(int i=0; i<pow(2, nums.size()); i++)
-        {
-            vector<int>temp;
-
-            for(int j=0; j<nums.size(); j++)
-            {
-                if(i & (1 << j)) temp.push_back(nums[j]);
-            }
-            subs.insert(temp);
-        }
-
-        for(auto it : subs)
-        {
-            ans.push_back(it);
-        }
-
+        vector<vector<int>>ans;
+        vector<int>temp;
+        helper(0, temp, nums, ans);
         return ans;
-
     }
 };
