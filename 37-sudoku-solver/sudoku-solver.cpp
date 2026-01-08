2aa62c3babd0e6f0b1620a2b2c63094f6937e11f
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2026-01-09 01:07:05 +0530

    Time: 777 ms (5.00%) | Memory: 84.3 MB (5.01%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,61 +1,61 @@
 class Solution {
 public:
-    bool isCheck(int row, int col, char num, vector<vector<char>>& board)
-    {
-        for(int i=0; i<9; i++)
-        {
-            if(board[i][col] == num) return false;
-            if(board[row][i] == num) return false;
-        }
-
-        int dr = row/3 * 3;
-        int dc = col/3 * 3;
-
-        for(int i=dr; i<dr+3; i++)
-        {
-            for(int j=dc; j<dc+3; j++)
-            {
-                if(board[i][j] == num) return false;
-            }
-        }
-        return true;
-    }
-    bool helper(int row, int col, vector<vector<char>>& board)
-    {
-        if(row == board.size()) return true;
-        int nRow = 0;
-        int nCol = 0;
-        if(col == board.size() - 1){
-            nRow = row + 1;
-            nCol = 0;
-        }
-        else
-        {
-            nRow = row;
-            nCol = col + 1;
-        }
-
-        if(board[row][col] != '.')
-        {
-            if(helper(nRow, nCol, board)) return true;
-        }
-        else
-        {
-            for(int i='1'; i<='9'; i++)
-            {
-                if(isCheck(row, col, i, board))
-                {
-                    board[row][col] = i;
-                    if(helper(nRow, nCol, board)) return true;
-                    else board[row][col] = '.';
+    vector<int> nextEmptySpace(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    return {i, j};
                 }
             }
         }
 
+        return {};
+    }
+    bool isSafe(int row, int col, char val, vector<vector<char>>& board){
+        // for the row
+        for(int c = 0; c < 9; c++){
+            if(board[row][c] == val){
+                return false;
+            }
+        }
+
+        // for the col
+        for(int r = 0; r < 9; r++){
+            if(board[r][col] == val){
+                return false;
+            }
+        }
+
+        // for the 3 * 3 blocks
+        int sRow = row / 3 * 3;
+        int sCol = col / 3 * 3;
+
+        for(int i=sRow; i<sRow+3; i++){
+            for(int j=sCol; j<sCol+3; j++){
+                if(board[i][j] == val){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    bool helper(vector<vector<char>>& board) {
+        auto pos = nextEmptySpace(board);
+        if (pos.empty()) {
+            return true;
+        }
+        int row = pos[0];
+        int col = pos[1];
+        for (char i = '1'; i <= '9'; i++) {
+            if (isSafe(row, col, i, board)) {
+                board[row][col] = i;
+                if(helper(board)) return true;
+                board[row][col] = '.';
+            }
+        }
         return false;
     }
     void solveSudoku(vector<vector<char>>& board) {
-        
-        helper(0, 0, board);
+        helper(board);
     }
 };
