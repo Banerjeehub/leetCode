3e307ea0b6ca58394ea6bf56c707001a08aed066
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2026-01-09 22:57:30 +0530

    Time: 351 ms (47.10%) | Memory: 11.9 MB (11.14%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    bool helper(int row, int col, int idx, string &word, vector<vector<char>>& board, vector<vector<int>>&visited){
-        if(idx == word.size()){
-            return true;
-        }
+    bool helper(int row, int col, int idx, int n, int m,
+                vector<vector<char>>& board, vector<vector<int>>& visited,
+                string word) {
+        if (idx == word.size()) return true;
 
-        int dr[4] = {1, -1, 0, 0};
-        int dc[4] = {0, 0, 1, -1};
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, 1, -1};
 
         for(int i=0; i<4; i++){
-            int nr = row + dr[i];
-            int nc = col + dc[i];
+            int nr = row + dx[i];
+            int nc = col + dy[i];
 
-            if(nr >= 0 && nr < board.size() && nc >= 0 && nc < board[0].size() && !visited[nr][nc] && board[nr][nc] == word[idx]){
+            if(nr >= 0 && nr < n && nc >= 0 && nc < m && !visited[nr][nc] && board[nr][nc] == word[idx]){
                 visited[nr][nc] = 1;
-                if (helper(nr, nc, idx+1, word, board, visited)) return true;
-                visited[nr][nc] = false;
+                if(helper(nr, nc, idx+1, n, m, board, visited, word)) return true;
+                visited[nr][nc] = 0;
             }
         }
+
         return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
+
         int n = board.size();
         int m = board[0].size();
-        vector<vector<int>>visited(n, vector<int>(m, 0));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(board[i][j] == word[0]){
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == word[0]) {
                     visited[i][j] = 1;
-                    if(helper(i, j, 1, word, board, visited)) return true;
+                    if (helper(i, j, 1, n, m, board, visited, word))
+                        return true;
                     visited[i][j] = 0;
                 }
             }
