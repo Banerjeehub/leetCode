8beae6569054f245bd7074bb4eb07af676ba331f
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2026-01-11 01:21:56 +0530

    Time: 2 ms (51.49%) | Memory: 8.1 MB (57.21%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,60 +1,27 @@
 class Solution {
 public:
-    bool isCheck(int row, int col, int n, vector<string>& board) {
-        int dr(row), dc(col);
-        // same row
-
-        while (dc >= 0) {
-            if (board[dr][dc] == 'Q')
-                return false;
-            dc--;
-        }
-
-        dr = row;
-        dc = col;
-
-        // upper diagonal
-
-        while (dr >= 0 && dc >= 0) {
-            if (board[dr][dc] == 'Q')
-                return false;
-            dr--;
-            dc--;
-        }
-
-        // lower diagonal
-        dr = row;
-        dc = col;
-        while (dc >= 0 && dr < n) {
-            if (board[dr][dc] == 'Q')
-                return false;
-            dr++;
-            dc--;
-        }
-
-        return true;
-    }
-    void solve(int col, int n, vector<string>& board,
-               int &ans) {
-        if (col == n) {
-            ans++;
+    void helper(int idx, int n, int &count,vector<int>&col, vector<int>&diag, vector<int>&antiDiag){
+        if(idx == n){
+            count++;
             return;
         }
 
-        for (int row = 0; row < n; row++) {
-            if (isCheck(row, col, n, board)) {
-                board[row][col] = 'Q';
-                solve(col + 1, n, board, ans);
-                board[row][col] = '.';
+        for(int i=0; i<n; i++){
+            if(!col[i] && !diag[idx-i + (n-1)] && !antiDiag[idx+i]){
+                col[i] = 1;
+                diag[idx-i+(n-1)] = 1;
+                antiDiag[idx+i] = 1;
+                helper(idx+1, n, count, col, diag, antiDiag);
+                col[i] = 0;
+                diag[idx-i+(n-1)] = 0;
+                antiDiag[idx+i] = 0;
             }
         }
     }
     int totalNQueens(int n) {
-
-        int ans = 0;
-        string str(n, '.');
-        vector<string> board(n, str);
-        solve(0, n, board, ans);
-        return ans;
+        vector<int>col(n, 0), diag(2*n, 0), antiDiag(2*n, 0);
+        int count = 0;
+        helper(0, n, count, col, diag, antiDiag);
+        return count;
     }
 };
