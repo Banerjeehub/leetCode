caea2ec849bfb7addb3c0565b7567697e4e606bd
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2026-01-25 23:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (97.71%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
+        // The main target is to find the lowest possible pos
+        // where we can put the target
+        // now as its sorted in an ascending order
+        // we can check if the current mid index is greater or equal to the target
+        // because if the current mid is greater or equal now then all the elements in the right has the possibility of being eligible for the index
+        // as if we are looking for the lowest possible index we will go to the left and discard the right.
+
+
         int low = 0;
         int high = nums.size() - 1;
-        int ans = -1;
+
         while(low <= high){
-            int mid = (low + high)/2;
+            int mid = (low + high) / 2;
             if(nums[mid] >= target){
                 high = mid - 1;
             }
             else low = mid + 1;
         }
-
         return low;
     }
 };
